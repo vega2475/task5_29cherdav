53248417698e284d9b41b6b2437ee27cab81c5e0
Nikolai <kolya.menshikov.03@inbox.ru>
2022-10-26 11:19:11 +0300

    release

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     //a
     private static int enterHeight() {
@@ -7,56 +8,35 @@ public class Main {
         return scanner.nextInt();
     }
 
-
-    private static void drawSticks(int weidth, int height){
+    private static void drawSticks(int weidth, int height) {
         System.out.print(" ");
 
-        for(int r = 0; r <= height; r++) {
+        for (int r = 0; r <= height; r++) {
 
             for (int c = 0; c <= weidth; c++) {
-                int i = 0;
-                int k = 0;
-                //boolean chechForEncrement = ((r > 0 && c > 0) && (r < height  && c < weidth )) && (((weidth - k) - i == c) && (r == i));
-
-                i++;
-
-                k++;
                 if ((r == 0 && c == 0) || (r == 0 && c == weidth - 1) || (r == weidth - 1 && c == 0) || (r == weidth - 1 && c == weidth - 1) || (r == (weidth - 1) / 2 && c == (weidth - 1) / 2)) {
-                    System.out.print("*");}
-                else if( r == 0 && (c > 0 && c < weidth - 1)) System.out.print(" ");
-                else if(c == 0 && (r > 0 && r < weidth - 1)) System.out.print("|");
-                else if(r == height - 1 && (c > 0 && c < weidth - 1)) System.out.print(" ");
-                else if(c == weidth - 1 && (r > 0 && r < weidth - 1)) System.out.print("|");
-                else if(  (r == c) && c!=weidth - 1 && r!=height - 1  ) System.out.print("\\");
-                else if (   ((r > 0 && c > 0) && (r < height  && c < weidth )) && (((weidth - k) - 1 == c) && (r == i))  )System.out.print("/");//нужно сделать так  чтобы когда эта была тру i и k увеличивались на 1, чем больше К тем больше пробелов от левой стороны у /
+                    System.out.print("*");
+                } else if (r == 0 && (c > 0 && c < weidth - 1)) System.out.print(" ");
+                else if (c == 0 && (r > 0 && r < weidth - 1)) System.out.print("|");
+                else if (r == height - 1 && (c > 0 && c < weidth - 1)) System.out.print(" ");
+                else if (c == weidth - 1 && (r > 0 && r < weidth - 1)) System.out.print("|");
+                else if ((r == c) && c != weidth - 1 && r != height - 1 && r != height && c != weidth)
+                    System.out.print("\\");
+                else if (((r > 0 && c > 0) && (r < height && c < weidth)) && (((weidth - r) - 1 == c)))
+                    System.out.print("/");//нужно сделать так  чтобы когда эта была тру i и k увеличивались на 1, чем больше К тем больше пробелов от левой стороны у /
                 else System.out.print(" ");
-                if(c == weidth - 1) System.out.println();
-
-
-
-
-
-
-
+                if (c == weidth - 1) System.out.println();
             }
         }
-
-
-
-
-
     }
-
 
     public static void main(String[] args) {
 
         int height = enterHeight();
         int weidth = height;
-        drawSticks(weidth, height);
-
-
-
-
-
+        if (height % 2 != 0) {
+            drawSticks(weidth, height);
+        }
+        else System.err.println("Error, input correct value ");
     }
 }
